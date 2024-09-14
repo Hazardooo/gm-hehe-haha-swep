c79cc0505f9aa0cee59e02bdffaee7156d9b30e0
Pavel <Hazdoo@outlook.com>
2024-09-14 15:51:34 +0300

    first commit

---
+++ hehe-haha-weapon/lua/weapons/weapon_hehe_haha_swep.lua
@@ -0,0 +1,59 @@
+if SERVER then
+    AddCSLuaFile()
+end
+
+if CLIENT then
+
+    SWEP.PrintName = "HEHE HAHA Swep"
+    SWEP.Author = "Hazardooo"
+    SWEP.Instructions = "Left mouse to 'HEHE HAHA'!"
+    SWEP.Slot = 0
+    SWEP.SlotPos = 1
+end
+
+SWEP.Category = "HEHE HAHA"
+SWEP.ViewModelFlip = false
+SWEP.ViewModelFOV = 60
+
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+SWEP.UseHands = false
+
+SWEP.ViewModel = "models/weapons/c_arms_hev.mdl"
+SWEP.WorldModel = ""
+
+SWEP.Weight = 1
+SWEP.AutoSwitchTo = true
+SWEP.AutoSwitchFrom = true
+
+SWEP.Primary.Recoil = 0
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
+
+--SWEP.ShootSound = Sound("weapons/laugh/laugh.wav")
+
+function SWEP:Initialize()
+    timer.Simple(0.2, function()
+        self:SetHoldType("idle")
+    end)
+    self:SetHoldType("idle")
+end
+
+function SWEP:SecondaryAttack()
+end
+
+function SWEP:PrimaryAttack()
+    self:SetNextPrimaryFire(CurTime() + 0.5)
+    self.Weapon:EmitSound("weapons/laugh/laugh.wav")
+end
+
+function SWEP:Reload()
+end
+
+
